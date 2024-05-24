7db04a42c0419395bef45d125143828d60033eba
Mateusz Urban <mateuszurban2@gmail.com>
2024-05-24 19:41:17 +0200

    communication between components works!

--- src/Component/AskName.purs
+++ src/Component/AskName.purs
@@ -4,19 +4,39 @@ import Prelude
 
 import Halogen as H
 import Halogen.HTML as HH
+import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
 
-component :: forall query input output m. H.Component query input output m
+-- This component can notify parent components of one event, `Clicked`
+data Output = NameChanged String
+
+type State = { currentName :: String}
+
+-- This component can handle one internal event, `Click`
+data Action = SubmitClicked | NameInputChanged String
+
+component :: forall query input m. H.Component query input Output m
 component =
   H.mkComponent
-    { initialState: identity
+    { initialState
     , render
     , eval: H.mkEval H.defaultEval
+      { handleAction = handleAction }
     }
   where
-  render :: forall state action. state -> H.ComponentHTML action () m
-  render _ = HH.div_ 
+  initialState :: input -> State
+  initialState _ = { currentName: ""}
+
+  handleAction :: Action -> H.HalogenM State Action () Output m Unit
+  handleAction = case _ of
+    NameInputChanged x ->  H.modify_ \_ -> { currentName : x}
+    SubmitClicked -> do
+      { currentName } <- H.get
+      H.raise $ NameChanged currentName
+
+  render :: State -> H.ComponentHTML Action () m
+  render _ = HH.div_
     [ HH.label_ [HH.text "Name"]
-    , HH.input [HP.type_ HP.InputText]
-    , HH.button [] [HH.text "Ok"]
+    , HH.input [HP.type_ HP.InputText, HE.onValueChange NameInputChanged]
+    , HH.button [HE.onClick \_ -> SubmitClicked] [HH.text "Ok"]
     ]
--- src/Page/Home.purs
+++ src/Page/Home.purs
@@ -8,11 +8,15 @@ import Prelude
 import Component.AskName as AskName
 import Component.Footer as Footer
 import Halogen as H
+import Halogen.HTML (output)
 import Halogen.HTML as HH
 import Type.Proxy (Proxy(..))
 
+type State = { currentName :: String}
+data Action = HandleAskName AskName.Output
+
 type Slots = 
-  ( askName :: forall query. H.Slot query Void Int 
+  ( askName :: forall query. H.Slot query AskName.Output Int 
   , footer :: forall query. H.Slot query Void Int 
   )
 _askName = Proxy :: Proxy "askName"
@@ -21,13 +25,22 @@ _footer = Proxy :: Proxy "footer"
 component :: forall query input output m. H.Component query input output m
 component =
   H.mkComponent
-    { initialState: identity
+    { initialState
     , render
     , eval: H.mkEval H.defaultEval
+      { handleAction = handleAction }
     }
   where
-  render :: forall state action. state -> H.ComponentHTML action Slots m
-  render _ = HH.div_ 
-    [ HH.slot_ _askName 0 AskName.component unit
-    , HH.slot_ _footer 0 Footer.component { name: "mateusz"}
+  initialState :: input -> State
+  initialState _ = { currentName: ""}
+
+  handleAction :: Action -> H.HalogenM State Action Slots output m Unit
+  handleAction = case _ of
+    HandleAskName (AskName.NameChanged x) ->
+      H.modify_ \_ -> { currentName : x}
+
+  render :: State -> H.ComponentHTML Action Slots m
+  render {currentName} = HH.div_ 
+    [ HH.slot _askName 0 AskName.component unit HandleAskName
+    , HH.slot_ _footer 0 Footer.component { name: currentName}
     ]
