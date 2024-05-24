a95839d6e0fa8cae7a974e469a680738784d8310
Mateusz Urban <mateuszurban2@gmail.com>
2024-05-24 19:07:28 +0200

    handle input changes

--- src/Component/Footer.purs
+++ src/Component/Footer.purs
@@ -1,7 +1,11 @@
 module Component.Footer (component) where
 
+import Data.Maybe
+import Prelude
 import Halogen as H
 import Halogen.HTML as HH
+
+data FooterAction = Receive FooterInput
 
 type FooterInput = { name :: String }
 
@@ -12,10 +16,20 @@ component =
   H.mkComponent
     { initialState
     , render
-    , eval: H.mkEval H.defaultEval
+    , eval: H.mkEval $ H.defaultEval
+        { handleAction = handleAction
+        , receive = Just <<< Receive
+        }
     }
   where
   initialState :: FooterInput -> FooterState
   initialState { name } = { name }
   render :: forall action. FooterState -> H.ComponentHTML action () m
   render {name} = HH.h1 [] [HH.text "witam ", HH.text name]
+  handleAction :: FooterAction -> H.HalogenM FooterState FooterAction () output m Unit
+  handleAction = case _ of
+    -- When we receive new input we update our `label` field in state.
+    Receive input ->
+    -- equivalent to
+    --   H.modify_ $ \x -> x { name = input.name }
+      H.modify_ _ { name = input.name }
